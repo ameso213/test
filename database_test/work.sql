16fd5ed2fbcbed1a60cc892e2c50893a9355af30
ameso213 <142228988+ameso213@users.noreply.github.com>
2024-03-11 16:36:54 +0300

    Add files via upload

---
+++ database_test/work.sql
@@ -0,0 +1,41 @@
+--CREATE TABLE students(
+--Reg_no INT  PRIMARY KEY,
+--First_Name TEXT ,
+--Last_Name TEXT ,
+--Couse TEXT,
+--Gender TEXT ,
+--D_O_B INT 
+--);
+
+--SELECT *FROM  students;
+
+--INSERT INTO students (Reg_no, First_Name, Last_Name, Course, Gender, d_o_b)
+--VALUES (1/2016,'John','Mutuku','DCS','Male', 13/6/1990),
+--(2/2016 ,'Steve', 'KipKoprir', 'DCS', 'Male',13/3/1985),
+--(3/2016 , 'Susan', 'Mutua', 'CIT', 'Female',19/11/1985),
+--(4/2017 , 'Steve', 'Kingori', 'DBIT', 'Male',1/19/1978)
+--;
+
+
+--ii)
+--SELECT * FROM students;
+--WHERE gender= 'Male';
+
+
+--V)
+--SELECT * FROM students;
+--WHERE gender='Male' BETWEEN 1980 AND 1989;
+
+
+
+--Vii)
+--SELECT * FROM students;
+--DELETE TABLE students (Reg_no, First_Name, Last_Name, Course, Gender, d_o_b)
+--VALUES (1/2016','John','Mutuku','DCS','Male',  13/6/1990),
+(2/2016 ,'Steve', 'KipKoprir', 'DCS', 'Male',  13/3/1985),
+(3/2016 , 'Susan', 'Mutua', 'CIT', 'Female',  19/11/1985),
+(4/2017 , 'Steve', 'Kingori', 'DBIT', 'Male',  1/19/1978)
+);
+
+
+
